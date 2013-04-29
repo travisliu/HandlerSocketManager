d69420bb45d388d0e979bbd06e60591c38921f93
travisliu <freewalker.tw@gmail.com>
2013-04-29 17:15:16 +1000

    commit first version source code

---
+++ Handler.php
@@ -0,0 +1,135 @@
+<?
+require 'HandlerSocketManager.php';
+
+/**
+ * 透過HandlerSocket對資料庫做新增刪除修改查詢的操作
+ */
+class Handler {
+
+	static private $dataBasePool = array();
+
+	/**
+	 * 資料庫名稱
+	 * @var String
+	 */
+    private $dataBase;
+    /**
+     * 資料表名稱
+     * @var String
+     */
+    private $table;
+    /**
+     * 索引名稱
+     * @var String
+     */
+    private $index;
+
+    /**
+     *
+     * @param String $dataBase 資料庫名稱
+     * @param String $table 資料表名稱
+     * @param String $index 索引名稱
+     */
+    public function __construct($dataBase, $table, $index){
+        
+        $this->dataBase = $dataBase;
+        $this->table    = $table;
+        $this->index    = $index;
+    }
+
+    /**
+     * 負責透過singleton的方式生產 Handler
+     * @param String $dataBase 資料庫名稱
+     * @param String $table 資料表名稱
+     * @param String $index 索引名稱，預設 primary key
+     * @return HandlerSocket $dbIndex 開啟index後的 HandlerSocket connection 
+     */
+    static public function factory($dataBase, $table, $index = HandlerSocket::PRIMARY){
+
+        $poolKey = $dataBase . '*' . $table . '*' . $index;
+        if(!isset(self::$dataBasePool[$poolKey])){
+            self::$dataBasePool[$poolKey] = new Handler($dataBase, $table, $index);
+        }
+
+        return self::$dataBasePool[$poolKey];
+    }
+	
+    /**
+     * 透過HandlerSocketManager
+     * 取得handlersocket 的instance
+     * @param Array $column 想要操作的欄位列表
+     * @param String $operating 操作模式 1 SELECT, 2 UPDATE, 3 INSERT, 4 DELETE
+     * @param String $index 索引名稱，預設 primary key
+     * @return HandlerSocket $instance 
+     */
+    private function getDBInstance($column, $operating){
+        $fields = implode(',', $column);
+        return HandlerSocketManager::factory($this->dataBase, $this->table, $fields, $this->index, $operating);
+    }
+
+    /**
+     * 搜尋單一個資料列
+     * @param Array $where
+     * @param Array $column
+     * @param String $op 操作方式，有以下選項, ‘=’, ‘>=’, ‘<=’, ‘>’, ‘<’, ‘+’。
+     * @return Array $result
+     */
+    public function findOne($where, $column, $op = '='){
+        
+        $db = $this->getDBInstance($column, HandlerSocketManager::$OPERATING_SELECT);
+        $rawResult = $db->executeSingle(HandlerSocketManager::$OPERATING_SELECT, $op, $where, 1, 0);
+        return $this->packRow($rawResult[0], $column);
+    }
+
+    /**
+     * 搜尋所有符合的資料列
+     * @param Array $where 搜尋條件
+     * @param Array $column 搜尋目標所需的欄位
+     * @param String $op 操作方式，有以下選項, ‘=’, ‘>=’, ‘<=’, ‘>’, ‘<’, ‘+’。
+     * @param Int $limit 搜尋結果列數的上限
+     * @param Int $skip 搜尋資料前忽略掉的行数
+     * @return Array $reslut 根據欄位名稱儲存對應值的array
+     */
+    public function findAll($where, $column, $op = '=', $limit = 999999, $skip = 0){
+        $db = $this->getDBInstance($column, HandlerSocketManager::$OPERATING_SELECT);
+        $rawResult = $db->executeSingle(HandlerSocketManager::$OPERATING_SELECT, $op, $where, $limit, $skip);
+        return $this->packAllResult($rawResult, $column);
+    }
+    /**
+     * 將所有搜尋結果包裝成相對應欄位的array
+     * @param String $rawList 要打包的原始資料 
+     * @param Int $column  相對應的欄位名稱
+     * @return array $result
+     */
+    private function packAllResult($rawList, $column){
+        $result = array();
+        foreach($rawList as $raw){
+            $result[] = $this->packRow($raw, $column);
+        }
+        return $result;
+    }
+    /**
+     * 將單一搜尋結果包裝成相對應欄位的array
+     * @param String $raw 要打包的原始資料 
+     * @param Int $column  相對應的欄位名稱
+     * @return array $result
+     */
+    private function packRow($raw, $column){
+
+        $rawRowNum = count($raw);
+        if($rawRowNum != count($column)){
+            throw new Exception('fail: lost fields');
+        }
+
+        $packResult = array();
+        for($i = 0;$i < $rawRowNum;$i++){
+            $resultKey = $column[$i];
+            $resultValue = $raw[$i];
+            $packResult[$resultKey] = $resultValue;
+        }
+
+        return $packResult;
+    }
+}
+
+?>
---
+++ HandlerSocketManager.php
@@ -0,0 +1,171 @@
+<?
+/**
+ * 主要負責 HandlerSocket 的連線管理
+ * 透過singleton的方式管理connection pool
+ * 由 factory function 製造 index
+ */
+class HandlerSocketManager extends HandlerSocket {
+
+	/**
+	 * 跟資料庫連線的IP 列表
+	 * @var array
+	 */
+    private $cluster;
+    /**
+     * 讀取與寫入使用的port r:read, w:write
+     * @var array
+     */
+    private $portList = array('r'=>9998, 'w'=>9999);
+
+    /**
+     * 負責暫存 handlersocket 的index 儲存池
+     * 主要是避免重復開啓 index
+     * @var array
+     */
+    static private $indexPool = array();
+    /**
+     * 負責儲存 handlersocket 的 singleton 儲存池
+     * @var array
+     */
+    static private $dbPool = array();
+
+    /**
+     * 對handlersocket 執行讀取操作的常數標誌
+     * @var array
+     */
+    static public $OPERATING_SELECT = 1;
+    /**
+     * 對handlersocket 執行更新操作的常數標誌
+     * @var array
+     */
+    static public $OPERATING_UPDATE = 2;
+    /**
+     * 對handlersocket 執行新增操作的常數標誌
+     * @var array
+     */
+    static public $OPERATING_INSERT = 3;
+    /**
+     * 對handlersocket 執行刪除操作的常數標誌
+     * @var array
+     */
+    static public $OPERATING_DELETE = 4;
+
+    public function __construct($dataBase, $mode){
+        
+        if(!array_key_exists($mode, $this->portList)){
+            throw new Exception('mode fail');
+        }
+
+        $this->init();
+        $host = $this->gethost($mode);
+        $port = $this->portList[$mode];
+
+        parent::__construct($host, $port);
+    }
+
+    private function init(){
+
+        $master = '192.168.1.1';
+        $slave = array(
+            array('host' => '192.168.1.2', 'proportion' => 1),
+            array('host' => '192.168.1.3', 'proportion' => 1)
+        );
+
+        $this->cluster['master'] = $master;
+        $this->cluster['slave'] = $slave;
+    }
+
+    /**
+     * 透過data base name 取得host
+     * @param String $mode 'w' => write or 'r' => read 
+     * @return String $host data base server ip
+     **/
+    private function getHost($mode = 'r'){
+
+        if($mode == 'r'){
+            $hostCandidate = $this->selectHost($this->cluster['slave']);
+            if(false != $hostCandidate){
+                $host = $hostCandidate;
+            }
+        }
+
+        if(empty($host)){
+            $host = $this->cluster['master'];
+        }
+
+        return $host;
+    }
+
+    /**
+     * select one host from cluster
+     * @param Array $cluster
+     * @return String the host ip address
+     **/
+    private function selectHost($cluster){
+
+        $tempCandidate=array();
+        if(is_array($cluster)){
+            foreach($cluster as $candidate){
+                for($i=0 ; $i < $candidate['proportion']; $i++){
+                    $tempCandidate[] = $candidate['host'];  
+                } 
+            }
+        }else{
+            return false;
+        }
+
+        if(0 == count($tempCandidate)){
+            return false;
+        }
+
+        return $tempCandidate[array_rand($tempCandidate)];
+    }
+
+    /**
+     *  產生 handlersocket->openIndex
+     * @param String $dataBase 資料庫名稱
+     * @param String $table 資料表明稱
+     * @param String $column 欄位名稱, 用逗號分隔
+     * @param String $index 索引名, 可以是手动创建的索引名。这个参数可为空，一般指定时是用于 SELECT
+     * @param int    $operating 1 SELECT, 2 UPDATE, 3 INSERT, 4 DELETE
+     * @param HandlerSocket $result 
+     * @return HandlerSocket $dbIndex 開啟index後的 HandlerSocket connection 
+     **/
+    static public function factory($dataBase, $table, $column, $index, $operating){
+
+        $mode = 'w';
+        if($operating == self::$OPERATING_SELECT){
+            $mode = 'r';
+        }
+
+        $dbPoolKey = $dataBase . '*' . $table;
+        $keyPoolKey = $dbPoolKey. '*' . $column . '*' . $index . '*' . $operating;
+        
+        if(!array_key_exists($dbPoolKey, self::$indexPool) || self::$indexPool[$dbPoolKey]['indexPoolKey'] != $keyPoolKey){
+
+            $db = self::getDBInstance($dataBase, $mode);
+            $result = $db->openIndex($operating, $dataBase, $table, $index, $column);
+            $dbInfo = array('indexPoolKey'=>$keyPoolKey, 'db'=>$db);
+            self::$indexPool[$dbPoolKey] = $dbInfo;
+        }
+
+        return self::$indexPool[$dbPoolKey]['db'];
+    }
+
+    /**
+	 * 透過singleton 的方式取得 HandlerSocket instance
+	 * @param String $dataBase 資料庫名稱
+	 * @param Char   $mode 操作方式 r: 讀, w: 寫
+     * @return HandlerSocket $instance 
+     **/
+    static public function getDBInstance($dataBase, $mode = 'r'){
+
+        $poolKey = $dataBase . '*' . $mode;
+        if(!isset(self::$dbPool[$poolKey])){
+            self::$dbPool[$poolKey] = new HandlerSocketManager($dataBase, $mode);
+        }
+
+        return self::$dbPool[$poolKey];
+    }
+}
+?>
